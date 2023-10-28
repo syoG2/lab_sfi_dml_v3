fadef9ea42c6f5e5a00d259ddec4908b187276e0
Kosuke-Yamada <ko5su2ke6_fykn@icloud.com>
2023-10-29 05:43:14 +0900

    register setting

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="sfidml",
+    version="0.1.0",
+    description="sfidml",
+    author="Kosuke Yamada",
+    packages=["sfidml"],
+    package_dir={
+        "sfidml": "sfidml",
+    },
+)
