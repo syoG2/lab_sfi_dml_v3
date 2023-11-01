b10f3ce247f5ed78e95064d1b87f0066113d581f
Kosuke-Yamada <ko5su2ke6_fykn@icloud.com>
2023-11-02 06:28:47 +0900

    complete to ranking

--- script/verb_clustering/evaluate_ranking-onestep.sh
+++ script/verb_clustering/evaluate_ranking-onestep.sh
@@ -23,20 +23,21 @@ split=test
 for setting in ${settings[@]}; do
     for model_name in ${model_names[@]}; do
         for ranking_method in ${ranking_methods[@]}; do
-            d1=${setting}/${pretrained_model_name}/${model_name}
-            d2=${vec_type2}/${clustering_name}/${clustering_method}
-            d3=${vec_type}
+            d1=${setting}
+            d2=${pretrained_model_name}/${model_name}
+            d3=${vec_type2}/${clustering_name}/${clustering_method}
+            d4=${vec_type}
             python ${source_dir}/evaluate_ranking.py \
-                --input_dir ${data_dir}/embedding/${d1} \
-                --input_params_file ${data_dir}/best_params_clustering/${d1}/${d2}/best_params.json \
-                --output_dir ${data_dir}/ranking/${d1}/${d3} \
+                --input_dir ${data_dir}/embedding/${d1}/${d2} \
+                --input_params_file ${data_dir}/best_params_clustering/${d1}/${d2}/${d3}/best_params.json \
+                --output_dir ${data_dir}/ranking/${d1}/${d2}/${d4} \
                 --ranking_method ${ranking_method} \
                 --split ${split}
 
             python ${source_dir}/evaluate_ranking_overlap.py \
-                --input_score_file ${data_dir}/ranking/${d1}/${d3}/score_${split}.jsonl \
-                --input_train_file ${data_dir}/embedding/${d1}/exemplars_train.jsonl \
-                --output_dir ${data_dir}/ranking_overlap/${d1}/${d3} \
+                --input_score_file ${data_dir}/ranking/${d1}/${d2}/${d4}/score_${split}.jsonl \
+                --input_train_file ${data_dir}/dataset/${d1}/exemplars_train.jsonl \
+                --output_dir ${data_dir}/ranking_overlap/${d1}/${d2}/${d4} \
                 --split ${split}
         done
     done
