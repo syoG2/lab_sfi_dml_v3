70be9592c8ad04dacd4543aa32835e2a5b682d83
syoG2 <syo53594f@gmail.com>
2025-01-19 01:36:22 +0900

    シェルスクリプトを見やすくする

--- script/preprocessing/collect_c4.sh
+++ script/preprocessing/collect_c4.sh
@@ -6,11 +6,10 @@ data_dir=./data/preprocessing/c4/collect
 split_name="train"
 
 
-for ((file_id=0; file_id<10; file_id++));
-do
-formatted_file_id=$(printf "%05d" ${file_id})
-uv run python ${source_dir}/collect_c4.py\
-    --file_id ${file_id} \
-    --split_name ${split_name} \
-    --output_file "${data_dir}/${split_name}_${formatted_file_id}.jsonl"
+for ((file_id=0; file_id<10; file_id++));do
+    formatted_file_id=$(printf "%05d" ${file_id})
+    uv run python ${source_dir}/collect_c4.py\
+        --file_id ${file_id} \
+        --split_name ${split_name} \
+        --output_file "${data_dir}/${split_name}_${formatted_file_id}.jsonl"
 done
--- script/preprocessing/lu_classifier/train.sh
+++ script/preprocessing/lu_classifier/train.sh
@@ -6,18 +6,21 @@ data_dir=./data/preprocessing/lu_classifier
 pretrained_model=bert-base-uncased
 n_splits=5
 
-
+text_intput_styles=(sep token0 token00)
 # text_input_style=sep
 # text_input_style=token0
-text_input_style=token00
+# text_input_style=token00
 
-for ((part=0; part<n_splits; part++));
-do
-uv run python ${source_dir}/lu_classifier.py \
-    --part "${part}" \
-    --pretrained_model "${pretrained_model}" \
-    --output_model_dir "${data_dir}/models/${pretrained_model}/${text_input_style}/train/${n_splits}_${part}" \
-    --device "cuda:2" \
-    --mode "train" \
-    --text_input_style "${text_input_style}"
+
+device=cuda:2
+for text_input_style in "${text_intput_styles[@]}";do
+    for ((part=0; part<n_splits; part++));do
+        uv run python ${source_dir}/lu_classifier.py \
+            --part "${part}" \
+            --pretrained_model "${pretrained_model}" \
+            --output_model_dir "${data_dir}/models/${pretrained_model}/${text_input_style}/train/${n_splits}_${part}" \
+            --device ${device} \
+            --mode "train" \
+            --text_input_style "${text_input_style}"
+    done
 done
