ffaf564da9fe45cda84a9b469618f4036f4a0684
syoG2 <syo53594f@gmail.com>
2025-01-19 00:07:08 +0900

    FrameNetの前処理を修正

--- script/preprocessing/preprocess_framenet.sh
+++ script/preprocessing/preprocess_framenet.sh
@@ -3,9 +3,8 @@
 source_dir=./source/preprocessing
 data_dir=./data/preprocessing/framenet
 
-# [ ]:output_exemplarを確認
 uv run python ${source_dir}/preprocess_framenet.py\
     --input_file ${data_dir}/collect/exemplars.jsonl\
-    --output_exemplar_file ${data_dir}/preprocess/tmp1/exemplars.jsonl\
-    --output_wordlist_file ${data_dir}/preprocess/tmp1/word_list.jsonl\
-    --device cuda:2
+    --output_exemplar_file ${data_dir}/preprocess/exemplars.jsonl\
+    --output_wordlist_file ${data_dir}/preprocess/word_list.jsonl\
+    --device cuda:0
