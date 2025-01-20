cc4c8ef77779be7cc4e4d8c1818bd57219c58f69
syoG2 <syo53594f@gmail.com>
2025-01-20 18:38:55 +0900

    crossvalidationに関わるシェルスクリプトをまとめる

---
+++ script/preprocessing/lu_classifier/crossvalidation.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+source_dir=./source/preprocessing/lu_classifier
+data_dir=./data/preprocessing/lu_classifier
+
+pretrained_model=bert-base-uncased
+n_splits=5
+
+# text_input_styles=(sep token0 token00)
+# text_input_styles=(sep)
+# text_input_styles=(token0)
+text_input_styles=(token00)
+
+# modes=(distinct random)
+# modes=(distinct)
+modes=(random)
+
+
+device=cuda:1
+for mode in "${modes[@]}";do
+    for text_input_style in "${text_input_styles[@]}";do
+        for ((part=0; part<n_splits; part++));do
+            uv run python ${source_dir}/lu_classifier.py \
+                --part "${part}" \
+                --n_splits "${n_splits}" \
+                --pretrained_model "${pretrained_model}" \
+                --input_dir "${data_dir}/dataset" \
+                --output_model_dir "${data_dir}/models/${pretrained_model}/${text_input_style}/${mode}/${n_splits}_${part}" \
+                --device ${device} \
+                --mode "${mode}" \
+                --text_input_style "${text_input_style}"
+        done
+    done
+done
