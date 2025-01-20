8f963dbf64d296b4bef51a71870d8cb0b4285340
syoG2 <syo53594f@gmail.com>
2025-01-20 18:38:21 +0900

    変数を切り出す

--- script/preprocessing/lu_classifier/train.sh
+++ script/preprocessing/lu_classifier/train.sh
@@ -6,21 +6,27 @@ data_dir=./data/preprocessing/lu_classifier
 pretrained_model=bert-base-uncased
 n_splits=5
 
-text_intput_styles=(sep token0 token00)
-# text_input_style=sep
-# text_input_style=token0
-# text_input_style=token00
+# text_input_styles=(sep token0 token00)
+# text_input_styles=(sep)
+# text_input_styles=(token0)
+text_input_styles=(token00)
+
+modes=(train)
 
 
-device=cuda:2
-for text_input_style in "${text_intput_styles[@]}";do
-    for ((part=0; part<n_splits; part++));do
-        uv run python ${source_dir}/lu_classifier.py \
-            --part "${part}" \
-            --pretrained_model "${pretrained_model}" \
-            --output_model_dir "${data_dir}/models/${pretrained_model}/${text_input_style}/train/${n_splits}_${part}" \
-            --device ${device} \
-            --mode "train" \
-            --text_input_style "${text_input_style}"
+device=cuda:0
+for mode in "${modes[@]}";do
+    for text_input_style in "${text_input_styles[@]}";do
+        for ((part=0; part<n_splits; part++));do
+            uv run python ${source_dir}/lu_classifier.py \
+                --part "${part}" \
+                --n_splits "${n_splits}" \
+                --pretrained_model "${pretrained_model}" \
+                --input_dir "${data_dir}/dataset" \
+                --output_model_dir "${data_dir}/models/${pretrained_model}/${text_input_style}/${mode}/${n_splits}_${part}" \
+                --device ${device} \
+                --mode "${mode}" \
+                --text_input_style "${text_input_style}"
+        done
     done
 done
--- script/preprocessing/preprocess_c4.sh
+++ script/preprocessing/preprocess_c4.sh
@@ -9,18 +9,18 @@ model_dir=./data/preprocessing/lu_classifier/models/bert-base-uncased/${text_inp
 
 split_name="train"
 
-
+device=cuda:3
 for ((file_id=5; file_id<6; file_id++));do
-    for ((part_id=270; part_id<360; part_id += 1));do
+    for ((part_id=300; part_id<350; part_id += 1));do
         formatted_file_id=$(printf "%05d" ${file_id})
         uv run python ${source_dir}/preprocess_c4.py\
             --input_file "${data_dir}/${split_name}_${formatted_file_id}.jsonl" \
             --file_id ${file_id} \
             --part_id ${part_id} \
             --split_name ${split_name} \
-            --output_exemplar_dir "${output_dir}/${split_name}_${formatted_file_id}"/lu\
-            --output_wordlist_file "${output_dir}/${split_name}_${formatted_file_id}/word_list/word_list_${part_id}.jsonl"\
-            --device "cuda:0" \
+            --output_exemplar_dir "${output_dir}/${split_name}_${formatted_file_id}"/lu \
+            --output_wordlist_dir "${output_dir}/${split_name}_${formatted_file_id}/word_list" \
+            --device ${device} \
             --model_path "${model_dir}/best_model"\
             --tokenizer_path "${model_dir}/tokenizer"\
             --text_input_style ${text_input_style}
