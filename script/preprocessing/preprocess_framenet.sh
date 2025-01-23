c005dea3f305c66be093fa6de70ddbeffffbb7ae
syoG2 <syo53594f@gmail.com>
2025-01-24 00:47:02 +0900

    verbの取得方法を編集する

--- script/preprocessing/preprocess_framenet.sh
+++ script/preprocessing/preprocess_framenet.sh
@@ -3,8 +3,11 @@
 source_dir=./source/preprocessing
 data_dir=./data/preprocessing/framenet
 
-uv run python ${source_dir}/preprocess_framenet.py\
-    --input_file ${data_dir}/collect/exemplars.jsonl\
-    --output_exemplar_file ${data_dir}/preprocess/exemplars.jsonl\
-    --output_wordlist_file ${data_dir}/preprocess/word_list.jsonl\
-    --device cuda:0
+verb_form=original
+# verb_form=lemma
+uv run python ${source_dir}/preprocess_framenet.py \
+    --input_file ${data_dir}/collect/exemplars.jsonl \
+    --output_exemplar_file ${data_dir}/preprocess/${verb_form}/exemplars.jsonl \
+    --output_wordlist_file ${data_dir}/preprocess/${verb_form}/word_list.jsonl \
+    --device cuda:0 \
+    --verb_form ${verb_form}
