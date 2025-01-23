696974439cfc314f80726f1c0fd06bd94ff252e1
syoG2 <syo53594f@gmail.com>
2025-01-24 00:50:46 +0900

    追加

--- script/verb_clustering_c4/find_best_params_clustering-twostep.sh
+++ script/verb_clustering_c4/find_best_params_clustering-twostep.sh
@@ -1,37 +1,51 @@
-#!/bin/bash
+#!/bin/bash\
 
 source_dir=./source/verb_clustering_c4
 data_dir=./data/verb_clustering_c4
 
 settings=(all_3_0 all_3_1 all_3_2)
-# settings=(all_3_1)
 
 pretrained_model_name=bert-base-uncased
+# pretrained_model_name=bert-large-uncased
+# pretrained_model_name=roberta-base
+# pretrained_model_name=roberta-large
 
 # model_names=(vanilla softmax_classification adacos_classification)
-# run_numbers=(00)
-# model_names=(siamese_distance triplet_distance arcface_classification)
-# run_numbers=(00 01 02 03)
-model_names=(siamese_distance)
-run_numbers=(00 01 02 03)
+# model_names=(adacos_classification)
+# model_names=(softmax_classification)
+model_names=(vanilla)
+run_numbers=(00)
 
-vec_types=(word mask wm)
-# vec_types=(word)
-# vec_types=(mask)
-# vec_types=(wm)
+# model_names=(arcface_classification siamese_distance triplet_distance)
+# model_names=(arcface_classification)
+# model_names=(siamese_distance)
+# model_names=(triplet_distance)
+# run_numbers=(00 01 02 03)
+
+vec_types=(mask wm word)
+vec_types=(mask)
+vec_types=(wm)
+vec_types=(word)
 
 clustering_name=twostep
 clustering_method1=xmeans
 clustering_method2=average
+
+c4_rate=1
+
+add_method=c4first
+# add_method=ratio
+# add_method=sequential
 
 for setting in "${settings[@]}"; do
     for model_name in "${model_names[@]}"; do
         for vec_type in "${vec_types[@]}"; do
             d1=${setting}/${pretrained_model_name}/${model_name}
             d2=${vec_type}/${clustering_name}-${clustering_method1}-${clustering_method2}
+
             uv run python ${source_dir}/find_best_params_clustering.py \
-                --input_dir "${data_dir}/embedding/${d1}" \
-                --output_dir "${data_dir}/best_params_clustering/${d1}/${d2}" \
+                --input_dir "${data_dir}/embedding/${add_method}/${c4_rate}/${d1}" \
+                --output_dir "${data_dir}/best_params_clustering/${add_method}/${c4_rate}/${d1}/${d2}" \
                 --vec_type "${vec_type}" \
                 --run_numbers "${run_numbers[@]}" \
                 --clustering_name ${clustering_name} \
