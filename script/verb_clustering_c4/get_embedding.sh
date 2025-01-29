c5d04d1117ef30af3cfbc7dc431813923d5ba637
syoG2 <syo53594f@gmail.com>
2025-01-29 11:09:54 +0900

    引数を修正

--- script/verb_clustering_c4/get_embedding.sh
+++ script/verb_clustering_c4/get_embedding.sh
@@ -4,7 +4,7 @@ source_dir=./source/verb_clustering_c4
 data_dir=./data/verb_clustering_c4
 
 settings=(all_3_0 all_3_1 all_3_2)
-# settings=(all_3_0)
+settings=(all_3_0)
 
 pretrained_model_name=bert-base-uncased
 
@@ -12,45 +12,36 @@ vec_types=(mask word)
 # vec_types=(mask)
 # vec_types=(word)
 
-# model_names=(adacos_classification softmax_classification vanilla)
+model_names=(adacos_classification softmax_classification vanilla)
 # model_names=(adacos_classification)
 # model_names=(softmax_classification)
-model_names=(vanilla)
+# model_names=(vanilla)
 run_numbers=(00)
 
 # model_names=(arcface_classification siamese_distance triplet_distance)
-# model_names=(arcface_classification)
-# model_names=(siamese_distance)
-# model_names=(triplet_distance)
-# run_numbers=(00 01 02 03)
-
-# splits=(train dev test)
-# splits=(train)
-# splits=(dev)
-# splits=(test)
-
-# add_method=ratio
-# add_method=sequential
-# add_method=ratio_verb
-
-add_method=sequential_n_verb
-
-c4_rate=1
-
-device=cuda:0
+model_names=(arcface_classification)
+model_names=(siamese_distance)
+model_names=(triplet_distance)
+run_numbers=(00 01 02 03)
 
 
-add_method=c4first
-# add_method=c4first_verb
-c4_rate=1
-splits=(train dev test-c4 test-framenet) # add_method=c4firstの場合
-# splits=(train)
-# splits=(dev)
-# splits=(test-c4)
-# splits=(test-framenet)
+splits=(train dev test test-c4 test-framenet) # add_method=c4firstの場合
+splits=(test-framenet)
 
-verb_form=original
 # verb_form=lemma
+verb_form=original
+
+add_method=frequency_100
+# add_method=ratio
+
+# add_key=lu_name
+add_key=verb
+
+# clustering_dataset=c4first
+# clustering_dataset=mix
+
+c4_rate=1
+device=cuda:0
 
 for setting in "${settings[@]}"; do
     for model_name in "${model_names[@]}"; do
@@ -60,10 +51,10 @@ for setting in "${settings[@]}"; do
                     d1=${setting}
                     d2=${pretrained_model_name}/${model_name}/${vec_type}/${run_number}
                     uv run python ${source_dir}/get_embedding.py \
-                        --input_file "${data_dir}/dataset/${verb_form}/${add_method}/${c4_rate}/${d1}/exemplars_${split}.jsonl" \
+                        --input_file "${data_dir}/dataset/${verb_form}/${add_method}/${add_key}/${c4_rate}/${d1}/exemplars_${split}.jsonl" \
                         --input_params_file "${data_dir}/train_model/${verb_form}/${d1}/${d2}/params.json" \
                         --input_model_file "${data_dir}/train_model/${verb_form}/${d1}/${d2}/pretrained_model_last.pth" \
-                        --output_dir "${data_dir}/embedding/${verb_form}/${add_method}/${c4_rate}/${d1}/${d2}" \
+                        --output_dir "${data_dir}/embedding/${verb_form}/${add_method}/${add_key}/${c4_rate}/${d1}/${d2}" \
                         --device ${device}
                 done
             done
--- script/verb_clustering_c4/make_dataset.sh
+++ script/verb_clustering_c4/make_dataset.sh
@@ -14,17 +14,30 @@ n_splits=3
 # add_method=c4first_verb
 # add_method=ratio_verb
 # add_method=sequential_verb
-add_method=sequential_n_verb
+# add_method=sequential_n_verb
+
+
+# verb_form=lemma
+verb_form=original
+
+# add_method=frequency_100
+add_method=ratio
+
+# add_key=lu_name
+add_key=verb
+
+# clustering_dataset=c4first
+# clustering_dataset=mix
+
 
 c4_rate=1
 
-verb_form=original
-# verb_form=lemma
 
 uv run python ${source_dir}/make_dataset.py \
     --input_file ${input_dir}/${verb_form}/exemplars.jsonl \
-    --output_dir ${data_dir}/${verb_form}/${add_method} \
+    --output_dir ${data_dir}/${verb_form}/${add_method}/${add_key} \
     --setting_prefix ${setting_prefix} \
     --n_splits ${n_splits} \
-    --c4_rate ${c4_rate} \
-    --add_method ${add_method}
+    --c4_rate "${c4_rate}" \
+    --add_method ${add_method} \
+    --add_key ${add_key}
