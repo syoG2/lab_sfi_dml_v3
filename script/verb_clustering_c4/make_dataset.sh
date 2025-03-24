8f6b012f16969f8253657b590cd106471198c181
syoG2 <syo53594f@gmail.com>
2025-03-24 16:28:27 +0900

    データセットの作成

--- script/verb_clustering_c4/make_dataset.sh
+++ script/verb_clustering_c4/make_dataset.sh
@@ -16,20 +16,31 @@ n_splits=3
 # add_method=sequential_verb
 # add_method=sequential_n_verb
 
-
+# verb_form: 2段階クラスタリングにおける、1段階目の区分
+#   originalでは動詞の原型、lemmaではstanzaにおけるlemmaをもとに区分
 # verb_form=lemma
 verb_form=original
 
+# add_method: C4のデータの追加方法
+#   frequency_100: 用例の最大数を100として、現れた順に用例を追加する
+#   ratio: 各用例の出現頻度に基づいて、用例を追加する
 add_method=frequency_100
-# add_method=ratio
+add_method=ratio
 
+# add_key: FrameNetの用例の何を基準にしてC4のデータを追加するか
+#   lu_name: FrameNetの用例のlu_nameを基準にする
+#   verb: FrameNetの用例の動詞を基準にする
 # add_key=lu_name
 add_key=verb
 
+# clustering_dataset: クラスタリングの対象とするデータセット
+#   c4first: C4のデータを最初にクラスタリングしてから、FrameNetのデータを割り当てる
+#   mix: C4のデータとFrameNetのデータを混ぜてクラスタリングする
 # clustering_dataset=c4first
 # clustering_dataset=mix
 
-c4_rate=1
+# c4_rate: FrameNetの用例に対し、C4のデータを追加する割合
+c4_rate=2
 
 
 uv run python ${source_dir}/make_dataset.py \
